92974724fcc9920d969b32735e47bcccca69a6a7
Ridwanullah <aladebaruwa@gmail.com>
2024-12-08 12:59:36 +0100

    dark mode v1  🧑‍🍳

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-let cont = document.getElementsByClassName(".container");
+let cont = document.getElementById("container");
+let darkTog = document.getElementById("dark-mode");
+let lightTog = document.getElementById("icon");
+
+darkTog.addEventListener("click", darkInit);
+
+function darkInit() {
+    cont.classList.toggle("dark");
+    
+
+    // Toggle the icon between moon and sun
+    if (lightTog.classList.contains("bx-moon")) {
+        lightTog.classList.remove("bx-moon");
+        lightTog.classList.add("bx-sun");
+    } else {
+        lightTog.classList.remove("bx-sun");
+        lightTog.classList.add("bx-moon");
+    }
+}
