64eeb48bd3265aa8d077d6c8c01aaed2b774562a
Ridwanullah <aladebaruwa@gmail.com>
2024-12-12 19:43:46 +0100

    cooking

---
+++ js/dashboard.js
@@ -0,0 +1,7 @@
+let datEdit = document.getElementById("date-edit");
+
+datEdit.addEventListener("click", changeDate);
+
+function changeDate() {
+    const dateForm = document.getElementById("date-range-form");
+}
